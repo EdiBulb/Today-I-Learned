38ce65414979785a7d11d77a3d21ec0b88edd09a
Edibulb <gnsqud24@naver.com>
2025-01-29 23:07:33 +1300

    NumPy and Pandas

--- Data Analytics/iris/activity1.py
+++ Data Analytics/iris/activity1.py
@@ -1,20 +1,19 @@
 
 
-# 필요한 라이브러리
+# 필요한 라이브러리 불러오기
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_iris
 
 # 1. 데이터 로드
-iris = load_iris()
-df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+iris = load_iris() # iris 데이터를 불러온다.
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names) # iris.data는 꽃잎과 꽃받침의 길이/너비 정보를 포함하는 Numpy 배열, iris.feature_names 는 각 변수의 이름
 df['target'] = iris.target  # 레이블 추가 (0, 1, 2로 표시)
 
 # 2. 데이터 정보 출력
 print("=== Iris 데이터셋 ===")
 print(df.head())  # 데이터 샘플 확인
-print("\n=== 데이터 통계 ===")
 print(df.describe())  # 기초 통계 정보
 
 # 3. 각 클래스(target)의 분포 확인
@@ -22,7 +21,7 @@ print("\n=== 클래스 분포 ===")
 print(df['target'].value_counts())
 
 # 상관계수 계산
-correlation_matrix = df.corr()
+correlation_matrix = df.corr() # df.corr() 변수 간 상관계수를 계산한다.
 print("=== Iris 데이터 상관계수 ===")
 print(correlation_matrix)
 
